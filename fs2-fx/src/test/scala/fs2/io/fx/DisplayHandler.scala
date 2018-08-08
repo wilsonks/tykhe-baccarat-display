029d760104689d843758986b446bc9aef677edaf
Wilson <wilsonks@gmail.com>
2018-08-08 19:38:32 +0530

    Tie Win in the Beginning Handling

--- fs2-fx/src/test/scala/fs2/io/fx/DisplayHandler.scala
+++ fs2-fx/src/test/scala/fs2/io/fx/DisplayHandler.scala
@@ -74,6 +74,129 @@ class DisplayHandler(
   pairBetMin.textProperty().bindBidirectional(tPairBetMin.textProperty())
   pairBetMax.textProperty().bindBidirectional(tPairBetMax.textProperty())
 
+  beadRoad.getCountProperty
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          lastWinAnimation.play()
+          if (t2.longValue() > t1.longValue()) {
+            bigRoad.AddElement(beadRoad)
+          } else {
+            bigRoad.RemoveElement(beadRoad)
+          }
+          new AudioClip(getClass.getResource(beadRoad.LastWinAudio()).toExternalForm).play()
+          lastWin.setResult(beadRoad.LastWin())
+          totalCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          bigRoad.Reset()
+          lastWin.setResult(LastWinResult.EMPTY)
+          totalCount.setText("")
+        }
+        bigRoad.UpdatePredictions(b1, b2, b3, p1, p2, p3)
+      }
+    })
+
+  beadRoad.getBankerWinCount
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          bankerWinCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          bankerWinCount.setText("")
+        }
+      }
+    })
+
+  beadRoad.getPlayerWinCount
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          playerWinCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          playerWinCount.setText("")
+        }
+      }
+    })
+
+  beadRoad.getTieWinCount
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          tieWinCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          tieWinCount.setText("")
+        }
+      }
+    })
+
+  beadRoad.getBankerPairCount
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          bankerPairCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          bankerPairCount.setText("")
+        }
+      }
+    })
+
+  beadRoad.getPlayerPairCount
+    .addListener(new ChangeListener[Number] {
+      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
+        if (t2.intValue() > 0) {
+          playerPairCount.setText(String.valueOf(t2.intValue()))
+        } else {
+          playerPairCount.setText("")
+        }
+      }
+    })
+
+  bigRoad.bigEyeRoadListProperty
+    .addListener(new ChangeListener[ObservableList[BigEyeRoadLabel]] {
+      override def changed(
+        observableValue: ObservableValue[_ <: ObservableList[BigEyeRoadLabel]],
+        t: ObservableList[BigEyeRoadLabel],
+        t1: ObservableList[BigEyeRoadLabel]): Unit = {
+        if (!t1.isEmpty) bigEyeRoad.ReArrangeElements(t1)
+        else {
+          bigEyeRoad.Reset()
+        }
+      }
+    })
+
+  bigRoad.smallRoadListProperty
+    .addListener(new ChangeListener[ObservableList[SmallRoadLabel]] {
+      override def changed(
+        observableValue: ObservableValue[_ <: ObservableList[SmallRoadLabel]],
+        t: ObservableList[SmallRoadLabel],
+        t1: ObservableList[SmallRoadLabel]): Unit = {
+        if (!t1.isEmpty) smallRoad.ReArrangeElements(t1)
+        else {
+          smallRoad.Reset()
+        }
+      }
+    })
+
+  bigRoad.cockroachRoadListProperty
+    .addListener(new ChangeListener[ObservableList[CockroachRoadLabel]] {
+      override def changed(
+        observableValue: ObservableValue[_ <: ObservableList[CockroachRoadLabel]],
+        t: ObservableList[CockroachRoadLabel],
+        t1: ObservableList[CockroachRoadLabel]): Unit = {
+        if (!t1.isEmpty) cockroachRoad.ReArrangeElements(t1)
+        else {
+          cockroachRoad.Reset()
+        }
+      }
+    })
+
+  val lastWinAnimation: RotateTransition = new RotateTransition(Duration.millis(50), lastWin)
+  lastWinAnimation.setAxis(Rotate.Y_AXIS)
+  lastWinAnimation.setByAngle(180)
+  lastWinAnimation.setCycleCount(2)
+  lastWinAnimation.setInterpolator(Interpolator.LINEAR)
+  lastWinAnimation.setAutoReverse(true)
+
   display.root.addEventHandler(
     KeyEvent.KEY_PRESSED,
     new EventHandler[KeyEvent] {
@@ -244,129 +367,6 @@ class DisplayHandler(
       }
     }
   )
-
-  beadRoad.getCountProperty
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          lastWinAnimation.play()
-          if (t2.longValue() > t1.longValue()) {
-            bigRoad.AddElement(beadRoad)
-          } else {
-            bigRoad.RemoveElement(beadRoad)
-          }
-          new AudioClip(getClass.getResource(beadRoad.LastWinAudio()).toExternalForm).play()
-          lastWin.setResult(beadRoad.LastWin())
-          totalCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          bigRoad.Reset()
-          lastWin.setResult(LastWinResult.EMPTY)
-          totalCount.setText("")
-        }
-        bigRoad.UpdatePredictions(b1, b2, b3, p1, p2, p3)
-      }
-    })
-
-  beadRoad.getBankerWinCount
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          bankerWinCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          bankerWinCount.setText("")
-        }
-      }
-    })
-
-  beadRoad.getPlayerWinCount
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          playerWinCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          playerWinCount.setText("")
-        }
-      }
-    })
-
-  beadRoad.getTieWinCount
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          tieWinCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          tieWinCount.setText("")
-        }
-      }
-    })
-
-  beadRoad.getBankerPairCount
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          bankerPairCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          bankerPairCount.setText("")
-        }
-      }
-    })
-
-  beadRoad.getPlayerPairCount
-    .addListener(new ChangeListener[Number] {
-      override def changed(observableValue: ObservableValue[_ <: Number], t1: Number, t2: Number): Unit = {
-        if (t2.intValue() > 0) {
-          playerPairCount.setText(String.valueOf(t2.intValue()))
-        } else {
-          playerPairCount.setText("")
-        }
-      }
-    })
-
-  bigRoad.bigEyeRoadListProperty
-    .addListener(new ChangeListener[ObservableList[BigEyeRoadLabel]] {
-      override def changed(
-        observableValue: ObservableValue[_ <: ObservableList[BigEyeRoadLabel]],
-        t: ObservableList[BigEyeRoadLabel],
-        t1: ObservableList[BigEyeRoadLabel]): Unit = {
-        if (!t1.isEmpty) bigEyeRoad.ReArrangeElements(t1)
-        else {
-          bigEyeRoad.Reset()
-        }
-      }
-    })
-
-  bigRoad.smallRoadListProperty
-    .addListener(new ChangeListener[ObservableList[SmallRoadLabel]] {
-      override def changed(
-        observableValue: ObservableValue[_ <: ObservableList[SmallRoadLabel]],
-        t: ObservableList[SmallRoadLabel],
-        t1: ObservableList[SmallRoadLabel]): Unit = {
-        if (!t1.isEmpty) smallRoad.ReArrangeElements(t1)
-        else {
-          smallRoad.Reset()
-        }
-      }
-    })
-
-  bigRoad.cockroachRoadListProperty
-    .addListener(new ChangeListener[ObservableList[CockroachRoadLabel]] {
-      override def changed(
-        observableValue: ObservableValue[_ <: ObservableList[CockroachRoadLabel]],
-        t: ObservableList[CockroachRoadLabel],
-        t1: ObservableList[CockroachRoadLabel]): Unit = {
-        if (!t1.isEmpty) cockroachRoad.ReArrangeElements(t1)
-        else {
-          cockroachRoad.Reset()
-        }
-      }
-    })
-
-  val lastWinAnimation: RotateTransition = new RotateTransition(Duration.millis(50), lastWin)
-  lastWinAnimation.setAxis(Rotate.Y_AXIS)
-  lastWinAnimation.setByAngle(180)
-  lastWinAnimation.setCycleCount(2)
-  lastWinAnimation.setInterpolator(Interpolator.LINEAR)
-  lastWinAnimation.setAutoReverse(true)
 
   display.root.iconifiedProperty().values.subscribe(i => if (i) echo.cancel() else echo.restart())
   display.root.setOnCloseRequest(_ => {
